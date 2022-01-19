d1de9b3526ad7b31d09ec8e224e05ad13ff5a564
Pavel Koryakin <unclekoin@gmail.com>
2022-01-19 17:52:44 +0300

    feat: add cashReducer, customerReducer & redux-devtools-extension

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import { useDispatch, useSelector } from 'react-redux';
 
 const App = () => {
   const dispatch = useDispatch();
-  const cash = useSelector((state) => state.cash);
+  const cash = useSelector((state) => state.cash.cash);
 
   const addCash = (cash) => {
-    dispatch({ type: 'ADD_CASH', payload: cash });
-  };
+    dispatch({ type: 'ADD_CASH', payload: cash })
+  }
 
   const getCash = (cash) => {
-    dispatch({ type: 'GET_CASH', payload: cash });
-  };
+    dispatch({ type: 'GET_CASH', payload: cash })
+  }
 
   return (
     <div className="container text-center pt-5">
--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 import 'bootstrap/dist/css/bootstrap.css';
+import { store } from './store'
 import App from './App';
-
-const defaultState = {
-  cash: 0,
-};
-
-const reducer = (state = defaultState, action) => {
-  switch (action.type) {
-    case 'ADD_CASH':
-      return { ...state, cash: state.cash + action.payload };
-    case 'GET_CASH':
-      return { ...state, cash: state.cash - action.payload };
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
 
 ReactDOM.render(
   <Provider store={store}>
